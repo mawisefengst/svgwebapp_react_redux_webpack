2293ffbbf6b004c3cad0b18140b9926cfd508db7
Feng Ma <mawisefeng@gmail.com>
2016-10-23 11:16:47 -0400

    new server.js

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+const indexPath = path.join(__dirname, '/client/index.html')
+const publicPath = express.static(path.join(__dirname, '/dist'))
+app.use('/dist', publicPath)
+app.get('/', function (_, res) { res.sendFile(indexPath) })
 
-module.exports = {
-  app: function () {
-    const app = express()
-    const indexPath = path.join(__dirname, '/client/index.html')
-    const publicPath = express.static(path.join(__dirname, '/dist'))
-    app.use('/dist', publicPath)
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
-    return app
-  }
-}
+app.listen(process.env.PORT || 8080);
+
